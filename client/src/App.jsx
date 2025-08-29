1876ff976e7bb9c70d6bf1aac2df5eb27a112f6c
Ahmet Burak Karhan <aburakkarhan@gmail.com>
2025-08-29 12:45:06 +0300

    update: removed the localhost url from API variable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
-const API = import.meta.env.VITE_API_BASE;
+const API = "https://video-streaming-platform-pecb.onrender.com";
 
 function HomePage() {
   const [videos, setVideos] = useState([]);
